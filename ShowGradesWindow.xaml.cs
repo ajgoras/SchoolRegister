a95bffc1d2d1dbbf6836c5d09a50811bb284c4da
ajgoras <ajgoras@MacBook-Pro-ajgoras.local>
2022-03-03 18:46:14 +0100

    deleted unused code

--- ShowGradesWindow.xaml.cs
+++ ShowGradesWindow.xaml.cs
@@ -1,22 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace SchoolRegister
 {
-    /// <summary>
-    /// Logika interakcji dla klasy ShowGradesWindow.xaml
-    /// </summary>
     public partial class ShowGradesWindow : Window
     {
         public Student student;
